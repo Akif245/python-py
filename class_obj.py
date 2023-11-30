1393265591f43a4dd3d1b62c582702868c4fc55e
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-11-30 23:26:07 +0530

    class and object

--- class_obj.py
+++ class_obj.py
@@ -1,9 +1,10 @@
-class action:
+class act:
     name="alex"
     skill="shooting"
     hp="100"
     def info(self):
         print(f"{self.name}is the best{self.skill}")
-        a=action()
+        a=act()
         a.name="akif"
-        print(a)
+        a.skill="killing"
+        a.info()
