f13953d27d61c0508b3bed7c2b020621ddb0dc54
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-12-02 23:15:12 +0530

    Constructor

---
+++ constructor.py
@@ -0,0 +1,10 @@
+class fcn:
+    def __init__(self,n,o) :
+        print("Hello")
+        self.name=n
+        self.occup=o
+        def info (self):
+            print(f"{self.name}is{self.occup}")
+            a=fcn("Akif","student")
+            a.info()
+            
